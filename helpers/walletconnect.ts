4fa6f103b03453b278cef022caf3de1e278ff101
Matt Davison <45557982+matt-davison@users.noreply.github.com>
2022-04-09 16:53:53 -0400

    wallet connect signing and sc demo

--- helpers/walletconnect.ts
+++ helpers/walletconnect.ts
@@ -1,6 +1,7 @@
 import { formatJsonRpcRequest } from "@json-rpc-tools/utils";
 import WalletConnect from "@walletconnect/client";
 import QRCodeModal from "algorand-walletconnect-qrcode-modal";
+import algosdk from "algosdk";
 
 // Create a connector
 export const connector = new WalletConnect({
@@ -39,3 +40,32 @@ connector.on("disconnect", (error, payload) => {
         throw error;
     }
 });
+
+export async function requestSigning(txns: algosdk.Transaction[], connector: WalletConnect, walletConnectMsg: string): Promise<Uint8Array[] | null> {
+    const txnsToSign = txns.map(txn => {
+        const encodedTxn = Buffer.from(algosdk.encodeUnsignedTransaction(txn)).toString("base64");
+
+        return {
+            txn: encodedTxn,
+            message: walletConnectMsg,
+        };
+    });
+
+    const requestParams = [txnsToSign];
+    const request = formatJsonRpcRequest("algo_signTxn", requestParams);
+    const result: Array<string | null> = await connector.sendCustomRequest(request);
+    const decodedResult = result.map(element => {
+        return element ? new Uint8Array(Buffer.from(element, "base64")) : null;
+    });
+
+    if (decodedResult != null) {
+        const stxs: Uint8Array[] = []
+        decodedResult.forEach((elem) => {
+            if (elem != null) {
+                stxs.push(elem)
+            }
+        });
+        return stxs
+    }
+    return null
+}
